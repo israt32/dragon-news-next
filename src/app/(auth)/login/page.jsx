312bbda985f094ac74a48fa366288fa1f3cc3532
Israt Jahan <israt5227@gmail.com>
2026-05-04 00:44:30 +0600

    login error fixed

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -33,6 +33,14 @@ const LoginPage = () => {
 
     console.log(res, error)
 
+     if (error) {
+      alert(error.message);
+    }
+
+    if (res) {
+      alert("signIn successful");
+    }
+
   };
 
   return (
